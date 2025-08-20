4e9f058b33a278d5c88668941d44e98a26016cd0
MUHAMMED THABSHEER P <93212521+thabsheerporuthiyil@users.noreply.github.com>
2025-08-20 22:26:55 +0530

    Time: 0 ms (100.00%) | Memory: 52.4 MB (98.83%) - LeetSync

---
+++ 1293-three-consecutive-odds/three-consecutive-odds.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+    if(arr.length<3){
+        return false;
+    }
+    for(let i=0;i<arr.length-2;i++){
+        if(arr[i]%2!==0 && arr[i+1]%2!==0 && arr[i+2]%2!==0){
+            return true;
+        }
+    }
+    return false;
+};

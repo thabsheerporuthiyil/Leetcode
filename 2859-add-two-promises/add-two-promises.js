ff0f65690219161b0d99da027c2068e19f5b38c7
MUHAMMED THABSHEER P <93212521+thabsheerporuthiyil@users.noreply.github.com>
2025-09-09 21:42:34 +0530

    Time: 77 ms (6.26%) | Memory: 55.8 MB (6.38%) - LeetSync

--- 2859-add-two-promises/add-two-promises.js
+++ 2859-add-two-promises/add-two-promises.js
@@ -4,8 +4,8 @@
  * @return {Promise}
  */
 var addTwoPromises = async function(promise1, promise2) {
-    return Promise.all([promise1,promise2])
-    .then(([val1,val2])=>val1+val2)
+    const [val1,val2]=await Promise.all([promise1,promise2])
+    return val1+val2;
 };
 
 /**

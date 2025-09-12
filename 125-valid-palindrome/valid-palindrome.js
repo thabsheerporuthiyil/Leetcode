cd1c8c2237dc20fb2f911507a19a669b55e8a2c8
MUHAMMED THABSHEER P <93212521+thabsheerporuthiyil@users.noreply.github.com>
2025-09-12 21:47:02 +0530

    Time: 4 ms (77.29%) | Memory: 57.5 MB (67.79%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.js
@@ -0,0 +1,9 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isPalindrome = function(s) {
+    s=s.replace(/[^a-zA-Z0-9]/g, '').toLowerCase();
+    let reversed=s.split('').reverse().join('');
+    return s===reversed;
+};
